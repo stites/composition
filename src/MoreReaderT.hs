d4be3655e95ae54b82cb8d7824412bbe927f4315
stites <sam@stites.io>
2016-03-27 22:35:15 -0700

    reader and state

--- src/MoreReaderT.hs
+++ src/MoreReaderT.hs
@@ -1,6 +1,7 @@
 module ReaderT.More where
 
 import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
 import Control.Monad.Identity
 
 rDec :: Num a => Reader a a
@@ -8,6 +9,16 @@ rDec = ReaderT $ return . ((+) (-1))
 
 rShow :: Show a => ReaderT a Identity String
 rShow = ReaderT $ Identity . show
+
+rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
+rPrintAndInc = ReaderT $ \r -> do
+                          putStrLn $ "Hi: " ++ show r
+                          return   $ r + 1
+
+sPrintIncAccum :: (Num a, Show a) => StateT a IO String
+sPrintIncAccum = StateT $ \r -> do
+                           putStrLn $ "Hi: " ++ show r
+                           return   $ (show r, r + 1)
 
 
 
