1ab676f20d0d5a6f6ca91c5e20e43be5d6b4c9c4
stites <sam@stites.io>
2016-03-30 21:30:33 -0700

    add exciting things!

--- src/MoreReaderT.hs
+++ src/MoreReaderT.hs
@@ -1,8 +1,12 @@
-module ReaderT.More where
+module More.MonadTs where
 
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.State
+import Control.Monad.Trans.Maybe
 import Control.Monad.Identity
+import Control.Monad.IO.Class
+import Control.Monad
 
 rDec :: Num a => Reader a a
 rDec = ReaderT $ return . ((+) (-1))
@@ -20,5 +24,20 @@ sPrintIncAccum = StateT $ \r -> do
                            putStrLn $ "Hi: " ++ show r
                            return   $ (show r, r + 1)
 
+isValid :: String -> Bool
+isValid v = '!' `elem` v
 
+hungryText :: MaybeT IO String
+hungryText = do
+  v <- lift getLine
+  guard (isValid v)
+  return v
+
+main :: IO ()
+main = do
+  putStrLn "say something."
+  excite <- runMaybeT hungryText
+  case excite of
+    Nothing -> putStrLn "Nothing! Nothing was said!"
+    Just e  -> putStrLn ("Boom! what was said: " ++ e)
 
